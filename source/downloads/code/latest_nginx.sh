33db0d1c2fb7649f259f19a263a6ae2c3a2e905e
clay richardson <c.richardson917@gmail.com>
2013-12-23 22:09:37 -0600

    forgot one line in latest_nginx.sh

--- source/downloads/code/latest_nginx.sh
+++ source/downloads/code/latest_nginx.sh
@@ -1,6 +1,7 @@
 bash install nginx
 sudo apt-get update
 sudo apt-get install python-software-properites
+sudo add-apt-repository ppa:nginx/stable
 sudo apt-get update
 sudo apt-get install nginx
 nginx -v
